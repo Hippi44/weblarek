fd0265abecef25a4be65894cf915867e30e8dc05
Hippi <timple.games1@mail.ru>
2025-10-25 18:51:33 +0300

    Шаг: 2. Готовая архитектура

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,20 @@ export interface IApi {
     get<T extends object>(uri: string): Promise<T>;
     post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
 }
+
+interface IProduct {
+    id: string;
+    description: string;
+    image: string;
+    title: string;
+    category: string;
+    price: number | null;
+  }
+  
+  interface IBuyer {
+    payment: TPayment;
+    email: string;
+    phone: string;
+    address: string;
+  }
+  
