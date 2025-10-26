30171eb33cc14ace1e713295010fbd501aaae420
Hippi <timple.games1@mail.ru>
2025-10-26 15:56:28 +0300

    Шаг: 3. Готовая проверка, приступаю к шагу: 4.

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,11 @@ import { ShoppingCart } from './components/base/Models/ShoppingCart.ts';
 import { Customer } from './components/base/Models/Customer.ts';
 import { apiProducts } from './utils/data.ts';
 
+const productCatalogModel = new ProductCatalog();
+productCatalogModel.setItems(apiProducts.items);
+const shoppingCartModel = new ShoppingCart();
+const customerModel = new Customer();
+
+console.log("Массив товаров из каталога: ", productCatalogModel.getItems());
+console.log("Корзина: ", shoppingCartModel.getItems());
+console.log("Покупатель: ", customerModel.getData());
